c829fd65dcf03b79dfb29c5c77400869bccfe190
Ignacio Vergara Lecue <vergaral@lvergara.esrf.fr>
2020-10-12 17:19:35 +0200

    new comments on mxcube notes

--- MxCUBE-CodeNotes.py
+++ MxCUBE-CodeNotes.py
@@ -39,9 +39,13 @@ How/when to use new data : example , new beam position/calibration:
 warn user with colors in table.
 When this changes apply?? when created?? when saved ??
 
+
 If a change is created but not explicity saved, use new data ??
 show message to warn user ??
 Apply / save button like in 'normal' applications ??
+
+/!\ DONE!!
+
 ---------------------------------------
 
 @HOME: SAVE CALIBRATION/BEAM POSITION DATA TO XML!!
@@ -419,7 +423,7 @@ And dispatcher is part of 'Louie', that is also part of Bliss
 MXCuBE and BLISS Share the same signal/slot system
 
 ##############################################################################
-MXCuBE IDE SIGNAL/SLOTS : SIGNAL/SLOTS IN GUI CREATOR IDE BETWEEN BRICKS
+MXCuBE IDE GUI SIGNAL/SLOTS : SIGNAL/SLOTS IN GUI CREATOR IDE BETWEEN BRICKS
 ##############################################################################
  in class Connectable
  def define_signal
@@ -616,7 +620,7 @@ class HardwareObject(HardwareObjectNode, HardwareObjectMixin):
 
 class Equipment(HardwareObject, DeviceContainer):
 
-class DeviceContainer:
+class DeviceContainer(object):
 ---------------------------------
 class Device(HardwareObject)
         NB Deprecated - should be replaced by AbstractActuator
